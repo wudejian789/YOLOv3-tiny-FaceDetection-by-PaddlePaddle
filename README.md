c4c121b0933b487ab33d07f397e76a36671b37fb
Dante <793729558@qq.com>
2019-06-10 21:30:46 +0800

    YOLOv3_tiny Project

---
+++ README.md
@@ -0,0 +1,92 @@
+YOLOv3-tiny-FaceDetection-by-PaddlePaddle
+===
+![YOLOv3-tiny](https://github.com/wudejian789/YOLOv3-tiny-FaceDetection-by-PaddlePaddle/blob/master/YOLOv3-tiny.png)
+>Note: If there's any error like "No module named 'xxx'", please use command "pip install xxx" to repair.   
+
+Feel free to add my QQ: ***793729558*** to discuss with me.  
+Also you can add the QQ group: ***647303915*** to discuss together.  
+
+# 1. Import the module
+```python
+from WIDER import *
+from detection import *
+```
+# 2. How to load the data
+```python
+trainData = WIDER(imgPath='xxx/images',
+                  imgBboxGt='xxx/wider_face_train_bbx_gt.txt'
+                  maxBoxNum=256)
+```
+>***imgPath*** is your img root path;  
+>***imgBboxGt*** is your "..._bbx_gt.txt" path;  
+>***maxBoxNum*** is the maximum number of faces in each picture;  
+
+The storage structure of image data is based on WIDER data.   
+If you want to use your own data, please keep storage structure consistent or rewrite the data utils class.  
+# 3. How to train your model
+First you need to create a YOLOv3_tiny object.  
+```python
+model = YOLOv3_tiny(boxNum=64, use_cuda=True)
+``` 
+>***use_cuda*** is whether to use GPU;  
+
+Then you need to build the model structure.  
+```python
+model.build(boxNum=64)
+```
+>***boxNum*** is the maximum number of faces in each picture which should be consistent with WIDER's ***maxBoxNum***.    
+
+Next you can train your model.  
+```python
+model.train(imgClass=trainData, epoches=10, batchSize=64)
+```
+>***imgClass*** is your img data class object.  
+>***batchSize*** is the number of data used for each train step;  
+>***epoch*** is the total iteration number of your training data;  
+
+And the log will be print like follows:  
+```
+...
+After iterations 7400: loss = 52.402  mAP: 0.478  51.425 images//s  Estimated remaining time: 57995.182s
+After iterations 7500: loss = 32.706  mAP: 0.575  51.565 images//s  Estimated remaining time: 57775.328s
+After iterations 7600: loss = 17.977  mAP: 0.511  51.344 images//s  Estimated remaining time: 57961.978s
+...
+```
+Finally you need to save your model for future use.  
+```
+model.save('./infer_model')
+```
+>First parameter is the path of model saved.  
+
+Ok, I know you are too lazy to train your own model. Also you can use my trained model in `'/infer_model'`.  The model is trained on WIDER train data and score ***mAP = 0.779*** on WIDER val data.  
+# 4. How to use your model to detect face
+First you need to create a Detector object.  
+```python
+detector = Detector(modelPath='./infer_model',USE_CUDA=False)
+```
+>***modelPath*** is your model path;  
+>***USE_CUDA*** is whether to use GPU;  
+
+Then you can do face detection by call it.  
+```python
+imgs,bboxes_pre = detector(imgList=['imgs/1.jpg','imgs/2.png'],
+                          confidence_threshold=0.5,nms_threshold=0.3)
+```
+>***imgList*** is your image path list;
+>***confidence_threshold***  is confidence threshold in non-maximum suppression;  
+>***nms_threshold*** is nms threshold in non-maximum suppression;  
+
+It will return a list of each image and its detection result.  
+Also you can plot it and save result:  
+```
+for i,(img,bbox_pre) in enumerate(zip(imgs,bboxes_pre)):
+    draw_bbox(img, bbox_pre, savePath=f'imgs/{i+1}_out.png')
+``` 
+>***savePath*** is your image save path;  
+
+The results are shown below:  
+![result](https://github.com/wudejian789/YOLOv3-tiny-FaceDetection-by-PaddlePaddle/blob/master/imgs/res1.png)  
+![result](https://github.com/wudejian789/YOLOv3-tiny-FaceDetection-by-PaddlePaddle/blob/master/imgs/res2.png)
+![result](https://github.com/wudejian789/YOLOv3-tiny-FaceDetection-by-PaddlePaddle/blob/master/imgs/res3.png)
+![result](https://github.com/wudejian789/YOLOv3-tiny-FaceDetection-by-PaddlePaddle/blob/master/imgs/res4.png)
+![result](https://github.com/wudejian789/YOLOv3-tiny-FaceDetection-by-PaddlePaddle/blob/master/imgs/res5.png)
---
+++ imageSolver.py
@@ -0,0 +1,88 @@
+import os,re,random,cv2
+import numpy as np
+class WIDER:
+    def __init__(self, imgPath, imgBboxGt, maxBoxNum=64):
+        with open(imgBboxGt) as f:
+            txt = ''.join(f.readlines())
+            imgPathArray = np.array(re.findall(r'(.*?\.jpg)\n',txt))
+            numArray = np.array(re.findall(r'\.jpg\n(\d+)',txt),dtype='int')
+            bboxArray = np.array(re.findall(r'(\d+) (\d+) (\d+) (\d+).*?\n',txt),dtype='float')
+        numArray,bboxArray = self._expansion_rows_to_maxBoxNum(bboxArray,numArray,maxBoxNum)
+        self.imgPath,self.imgPathArray,self.bboxArray = imgPath,imgPathArray,bboxArray
+        self.maxBoxNum,self.imgNum = maxBoxNum,len(imgPathArray)
+    def random_batch_img_stream(self, batchSize=8):
+        idList = [i for i in range(self.imgNum)]
+        while True:
+            samples = random.sample(idList,batchSize) if batchSize>0 else random.sample(idList,len(idList))
+            yield self._scale_img_bbox_array(self.imgPath,self.imgPathArray[samples],self.bboxArray[samples])
+    def batch_img_stream(self, batchSize=8):
+        for i in range(self.imgNum//batchSize+1):
+            yield self._scale_img_bbox_array(self.imgPath,self.imgPathArray[i*batchSize:(i+1)*batchSize],self.bboxArray[i*batchSize:(i+1)*batchSize])  
+    def _expansion_rows_to_maxBoxNum(self, bboxArray, numArray, maxBoxNum):
+        obj = 0
+        for i in range(numArray.shape[0]):
+            if maxBoxNum>numArray[i]:
+                obj += numArray[i]
+                bboxArray = np.insert(bboxArray,obj=obj,values=np.array([[0,0,0,0] for j in range(maxBoxNum-numArray[i])]),axis=0)
+                obj += maxBoxNum-numArray[i]
+            else:
+                obj += maxBoxNum
+                bboxArray = np.delete(bboxArray,obj=[j for j in range(obj,obj+numArray[i]-maxBoxNum)],axis=0)
+                numArray[i] = maxBoxNum
+        bboxArray = bboxArray.reshape(-1,maxBoxNum,4)
+        return numArray,bboxArray
+    def _scale_img_bbox_array(self, path, imgPathArray, bboxArray, IMG_WH=416):
+        imgArray = []
+        for i in range(imgPathArray.shape[0]):
+            img = cv2.imread(os.path.join(path,imgPathArray[i]))
+            h,w,c = img.shape
+            dh,dh_e,dw,dw_e = 0,0,0,0
+            if w>h:
+                dh = (w-h)//2
+                dh_e = w-h-dh-dh
+            else:
+                dw = (h-w)//2
+                dw_e = h-w-dw-dw
+            img = cv2.copyMakeBorder(img,dh,dh+dh_e,dw,dw+dw_e,cv2.BORDER_CONSTANT,value=[0,0,0])
+            imgArray.append(cv2.resize(img,(IMG_WH,IMG_WH))/255.0)
+            bboxArray[i,:,2] = bboxArray[i,:,2]/(w+dw+dw+dw_e)
+            bboxArray[i,:,3] = bboxArray[i,:,3]/(h+dh+dh+dh_e)
+            bboxArray[i,:,0] = (bboxArray[i,:,0]+dw)/(w+dw+dw+dw_e) + bboxArray[i,:,2]/2
+            bboxArray[i,:,1] = (bboxArray[i,:,1]+dh)/(h+dh+dh+dh_e) + bboxArray[i,:,3]/2
+            
+        return np.array(imgArray,dtype='float32').swapaxes(1,3).swapaxes(2,3),np.array(bboxArray,dtype='float32')
+
+def draw_bbox(img, bboxes, show=True, color=(0,0,255), lineWidth=1, savePath=None):
+    if type(img)==str:
+        img = cv2.imread(img)
+    else:
+        if img.shape[0] < img.shape[1] and img.shape[0] < img.shape[2]:img = img.swapaxes(0,2).swapaxes(0,1)
+    if np.max(img)<=1:
+        img *= 255
+    img = img.astype('uint8')
+    for bbox in bboxes:
+        x1,y1,x2,y2 = bbox[:4]
+        img = cv2.rectangle(img, ( int(round(x1)),int(round(y1)) ), ( int(round(x2)),int(round(y2)) ), color, lineWidth)
+    if savePath is not None:
+        cv2.imwrite(savePath, img)
+    cv2.imshow("bbox",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def scale_img(img,IMG_WH=416):
+    if type(img)==str:
+        img = cv2.imread(img)
+    else:
+        if img.shape[0] < img.shape[1] and img.shape[0] < img.shape[2]:img = img.swapaxes(0,2).swapaxes(0,1)
+    h,w,c = img.shape
+    dh,dh_e,dw,dw_e = 0,0,0,0
+    if w>h:
+        dh = (w-h)//2
+        dh_e = w-h-dh-dh
+    else:
+        dw = (h-w)//2
+        dw_e = h-w-dw-dw
+    img = cv2.copyMakeBorder(img,dh,dh+dh_e,dw,dw+dw_e,cv2.BORDER_CONSTANT,value=[0,0,0])
+    imgArray = cv2.resize(img,(IMG_WH,IMG_WH))/255.0
+    return np.array(imgArray,dtype='float32').swapaxes(0,2).swapaxes(1,2)
